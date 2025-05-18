0727b517816073228ca17d336f1f7539963cfdef
camilo <143355526+Carpediem-code234@users.noreply.github.com>
2025-05-18 17:40:05 -0500

    feat: refactor home component with hero section and features, update styles for better layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  navigateToServices() {
+    console.log('Navegando a servicios');
+    // Esta función se llamará cuando se haga clic en el botón
+  }
 }
